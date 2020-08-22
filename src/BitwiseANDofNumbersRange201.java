b4537dc97a09a858eb665b0f9149f63a8d084bc8
yqxd <xuz19556@gmail.com>
2020-08-22 22:53:11 +0800

    1\2

---
+++ src/BitwiseANDofNumbersRange201.java
@@ -0,0 +1,7 @@
+class Solution202 {
+    public boolean isHappy(int n) {
+        System.out.println("asd");
+        return true;
+    }
+}
+
---
+++ src/HappyNumber202.java
@@ -0,0 +1,5 @@
+class Solution201 {
+    public int rangeBitwiseAnd(int m, int n) {
+        return 1;
+    }
+}
