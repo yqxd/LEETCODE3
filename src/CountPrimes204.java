e4d1fda514ea903f83aacbf4dd06a856e27f991a
yqxd <xuz19556@gmail.com>
2020-11-05 14:42:43 +0800

    4

--- src/CountPrimes204.java
+++ src/CountPrimes204.java
@@ -1,27 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class countPrimes {
-    public int countPrimes(int n) {
+
+public class CountPrimes204 {
+    public static void main(String[] args){
+        CountPrimes204 A = new CountPrimes204();
+        System.out.println(A.countPrimes(10));
+    }
+    private int countPrimes(int n) {
         List<Integer> list = new ArrayList<Integer>();
         List<Integer> result = new ArrayList<Integer>();
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             list.add(1);
         }
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             if (list.get(i) == 1) {
                 result.add(i);
                 for (int j = 2; j * i <= n; j++) {
-
+                    list.set(i*j, 0);
                 }
             }
         }
         return result.size();
-    }
-}
-
-public class CountPrimes204 {
-    public static void main(String[] args){
-        countPrimes A = new countPrimes();
     }
 }
