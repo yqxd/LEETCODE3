94158945c4992e5a9b5c65d81e74168e410b9216
yqxd <xuz19556@gmail.com>
2020-11-13 13:22:15 +0800

    1

--- src/BitwiseANDofNumbersRange201.java
+++ src/BitwiseANDofNumbersRange201.java
@@ -3,12 +3,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BitwiseANDofNumbersRange201 {
-    public static void main(String[] args) {
-        BitwiseANDofNumbersRange201 A = new BitwiseANDofNumbersRange201();
-        System.out.println(A.rangeBitwiseAnd(2, 4));
+    private int FindDigit(int m, int k){
+        return ((m >>> k) << k) + (1 << k);
     }
 
     private int rangeBitwiseAnd(int m, int n) {
-        return 1;
+        int result = m, digit = 1, newnum;
+        while (true){
+            newnum = FindDigit(m, digit);
+            if(newnum > n || result == 0){
+                break;
+            } else{
+                result = result & newnum;
+            }
+            digit += 1;
+        }
+        return result;
     }
+
+    public static void main(String[] args) {
+        BitwiseANDofNumbersRange201 A = new BitwiseANDofNumbersRange201();
+        System.out.println(A.rangeBitwiseAnd(5, 7));
+    }
+
 }
